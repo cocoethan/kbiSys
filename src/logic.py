376396f946ce9b86b59057e0e6aef1e7b595ee83
Ethan Coco <ethancoco1@outlook.com>
2023-03-23 14:52:02 -0400

    Create logic.py

---
+++ src/logic.py
@@ -0,0 +1,26 @@
+attrValsDict = {} #Dictionary for only attribute values
+
+attrDict = {} #Dicitionary for attributes and values
+hardDict = {} #Dictionary for hard constraints
+penDict = {} #Dicitonary for penalty logic
+possDict = {} #Dictionary for possibility logic
+quaDict = {} #Dicitonary for qualitative choice logic
+
+def fillDicts(attrDictTemp, hardDictTemp, penDictTemp, possDictTemp, quaDictTemp):
+    attrDict = attrDictTemp
+    hardDict = hardDictTemp
+    penDict = penDictTemp
+    possDict = possDictTemp
+    quaDict = quaDictTemp
+
+def existence():
+    print()
+
+def exemplify():
+    print()
+
+def optimize():
+    print()
+
+def omni():
+    print()
