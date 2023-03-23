011d02c4a581f6b4571615af0059f7fde7a1d78d
Maxwell Twardowski <maxtwardo@gmail.com>
2023-03-22 21:13:21 -0400

    Update gui.py
    added dicts

--- src/gui.py
+++ src/gui.py
@@ -3,6 +3,11 @@ from tkinter import filedialog
 from tkinter import scrolledtext
 from tkinter import ttk
 
+# TO-DO
+    # can there be multiple comma/number values in single preference line
+    # 
+
+
 #Set-up Window
 window = tk.Tk()
 window.title("kbiSys")
@@ -32,14 +37,70 @@ def generate():
     possStr = possTxt.get('1.0', tk.END)
     quaStr = quaTxt.get('1.0', tk.END)
 
-    print(attrStr)
-    print(hardStr)
-    print(penStr)
-    print(possStr)
-    print(quaStr)
+    #print(attrStr)
+    #print(hardStr)
+    #print(penStr)
+    #print(possStr)
+    #print(quaStr)
+
+    #print(hardStr, type(hardStr))
+
+    #attributes dict creation (key is left, items are right)
+    attrStr = attrStr.split('\n')
+    attrDict = {}
+    for strs in attrStr:
+        if strs:     # add error checking later
+            strs = strs.replace(" ", "")
+            strs = strs.split(':')
+            #print(strs)
+            strs[1] = strs[1].split(',')
+            attrDict[strs[0]] = strs[1]
+    print(attrDict)
+        # do binary encoding
+
+    #hard constraints dict creation
+    hardStr = hardStr.split('\n')
+    hardDict = {}
+    for key, strs in enumerate(hardStr):
+        if strs:
+            hardDict[key] = strs
+    print(hardDict)
+
+    #penalty dict creation (delim on commma)
+    penStr = penStr.split('\n')
+    penDict = {}
+    for key, strs in enumerate(penStr):
+        if strs:
+            strs = strs.split(",")     # add error checking later
+            strs[1] = strs[1].replace(" ", "")
+            penDict[key] = strs
+    print(penDict)
+
+    #possibilistic dict creation (delim on comma)
+    possStr = possStr.split('\n')
+    possDict = {}
+    for key, strs in enumerate(possStr):
+        if strs:
+            strs = strs.split(",")  # add error checking later
+            strs[1] = strs[1].replace(" ", "")
+            possDict[key] = strs
+    print(possDict)
+
+    #Qualitative choice logic dict creation
+    quaStr = quaStr.split('\n')
+    quaDict = {}
+    for key, strs in enumerate(quaStr):
+        if strs:     # add error checking later
+            quaDict[key] = strs
+    print(quaDict)
+
 
 def clear():
     print('Cancelled...')
+
+#Can Delete?
+def handleChecks():
+    print("Handle Checks called:", exisVal.get(), exemVal.get(), optiVal.get(), omniVal.get())
 
 def helpIndex():
     print("Help Index Selected")
