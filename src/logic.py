79724660652e9509f222cb08a32a8e3ca21c7d63
Ethan Coco <ethancoco1@outlook.com>
2023-03-23 18:26:29 -0400

    Update logic.py

--- src/logic.py
+++ src/logic.py
@@ -1,3 +1,6 @@
+from numpy import binary_repr
+
+objects = {}
 attrValsDict = {} #Dictionary for only attribute values
 
 attrDict = {} #Dicitionary for attributes and values
@@ -12,6 +15,26 @@ def fillDicts(attrDictTemp, hardDictTemp, penDictTemp, possDictTemp, quaDictTemp
     penDict = penDictTemp
     possDict = possDictTemp
     quaDict = quaDictTemp
+
+    for i, key in enumerate(attrDict):
+        attrValsDict[i] = attrDict[key]
+    print(attrValsDict)
+
+    genObjects(i + 1)
+
+def genObjects(numOfAtts):
+    loopcnt = (2 ** numOfAtts)
+    for i in range(loopcnt):
+        tempList = list(binary_repr(i, numOfAtts))#
+        valList = list(range(numOfAtts))
+        for j in range(numOfAtts):
+            tempArr = attrValsDict[(numOfAtts - (j + 1))]
+            if(tempList[(numOfAtts - (j + 1))] == '0'):
+                valList[j] = tempArr[1]
+            elif(tempList[(numOfAtts - (j + 1))] == '1'):
+                valList[j] = tempArr[0]
+        keyStr = 'o' + str(i)
+        objects[keyStr] = valList
 
 def existence():
     print()
