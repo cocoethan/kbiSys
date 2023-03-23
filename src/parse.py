d6dfe443d0eedf4b57d9d480ca69462b5ad10a42
Ethan Coco <ethancoco1@outlook.com>
2023-03-23 14:51:35 -0400

    Create parse.py

---
+++ src/parse.py
@@ -0,0 +1,54 @@
+from logic import fillDicts
+
+attrDict = {} #Dicitionary for attributes and values
+hardDict = {} #Dictionary for hard constraints
+penDict = {} #Dicitonary for penalty logic
+possDict = {} #Dictionary for possibility logic
+quaDict = {} #Dicitonary for qualitative choice logic
+
+def parseInput(attrStr, hardStr, penStr, possStr, quaStr):
+    # attributes dict creation (key is left, items are right)
+    attrStr = attrStr.split('\n')
+    for strs in attrStr:
+        if strs:  # add error checking later
+            strs = strs.replace(" ", "")
+            strs = strs.split(':')
+            # print(strs)
+            strs[1] = strs[1].split(',')
+            attrDict[strs[0]] = strs[1]
+    print(attrDict)
+    # do binary encoding
+
+    # hard constraints dict creation
+    hardStr = hardStr.split('\n')
+    for key, strs in enumerate(hardStr):
+        if strs:
+            hardDict[key] = strs
+    print(hardDict)
+
+    # penalty dict creation (delim on commma)
+    penStr = penStr.split('\n')
+    for key, strs in enumerate(penStr):
+        if strs:
+            strs = strs.split(",")  # add error checking later
+            strs[1] = strs[1].replace(" ", "")
+            penDict[key] = strs
+    print(penDict)
+
+    # possibilistic dict creation (delim on comma)
+    possStr = possStr.split('\n')
+    for key, strs in enumerate(possStr):
+        if strs:
+            strs = strs.split(",")  # add error checking later
+            strs[1] = strs[1].replace(" ", "")
+            possDict[key] = strs
+    print(possDict)
+
+    # Qualitative choice logic dict creation
+    quaStr = quaStr.split('\n')
+    for key, strs in enumerate(quaStr):
+        if strs:  # add error checking later
+            quaDict[key] = strs
+    print(quaDict)
+
+    fillDicts(attrDict, hardDict, penDict, possDict, quaDict)
